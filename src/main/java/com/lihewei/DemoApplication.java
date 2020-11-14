be5b6c7c086d7f9ddeac9b7d386f846edcad7131
apple <1968895380@qq.com>
2020-11-14 17:43:14 +0800

    add say method

--- src/main/java/com/lihewei/DemoApplication.java
+++ src/main/java/com/lihewei/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.lihewei.demo;
+package com.lihewei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/lihewei/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.lihewei.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/say")
+    public String say(){
+        return "hello";
+    }
+}
