9ebcd92a7bb8a63ab9fd16ac64fe227a1930e998
apple <1968895380@qq.com>
2020-11-14 17:47:04 +0800

    add say method

--- src/main/java/com/lihewei/controller/TestController.java
+++ src/main/java/com/lihewei/controller/TestController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author apple
+ * @author lihewei
+ *
  */
 @RestController
 public class TestController {
