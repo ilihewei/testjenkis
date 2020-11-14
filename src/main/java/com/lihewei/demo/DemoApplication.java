cbbf5e42dd9c3711bf2bf14024e17f60fa7399fe
apple <1968895380@qq.com>
2020-11-14 17:19:14 +0800

    test

---
+++ src/main/java/com/lihewei/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.lihewei.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//asdasd
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
